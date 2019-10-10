5b5683a40eec34a049d5e15eee2729ec0b1c6f20
Daemon <makushin.dima@gmail.com>
2019-10-10 14:28:46 +0300

    del: file config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-exports.my_token = '667129519:AAH5tNcVmmv3hA9aVQpFFXp86YKfLwg3s-s';
-exports.admins_user = [187518808, 134809187, 500638605];
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
-const data = require('./config');
+var moment = require('moment');
+const data = require('./config.json');
+
+// data for work bot
+const token = data.token;
+const admins = data.admins_user;
+
+const bot = new TelegramBot(token, { polling: true, filepath: false });
+
+// add sql-lite database
 const sqlite = require('sqlite-sync');
 
 sqlite.connect('statistic.db');
@@ -9,6 +18,7 @@ sqlite.run(`CREATE TABLE IF NOT EXISTS log(
     chat_id INTEGER,
     message_id INTEGER,
     type TEXT,
+    type_log TEXT,
     message TEXT,
     error TEXT,
     date INTEGER
@@ -17,23 +27,46 @@ sqlite.run(`CREATE TABLE IF NOT EXISTS log(
         throw res.error;
 });
 
-
-// data for work bot
-const token = data.my_token;
-const admins = data.admins_user;
-
-const bot = new TelegramBot(token, { polling: true, filepath: false });
-
+// setting bot
 bot.onText(/\/setting/, (msg) => {
     const chatId = msg.chat.id;
     const id = msg.from.id;
+    let datetime = Date.now();
     if (admins.indexOf(id) > 0) {
-        const log = sqlite.run('SELECT * FROM log');
-        console.log(JSON.stringify(log));
-        bot.sendMessage(msg.from.id, 'statistic.\n');
+        // const log = sqlite.run('SELECT * FROM log');
+        const start = sqlite.run('SELECT date FROM log WHERE `id` = 1');
+        let del_message = 0;
+        let error_massage = 0;
+        let all_message = 0;
+        let statistic = '';
+        // console.log('log: \n', JSON.stringify(log));
+
+        bot.sendMessage(id, 'Список команд:\n' +
+            '/setting - вызов данного меню\n' +
+            '/statistic all - вся статистика за весь период работы; \n' +
+            '/error message - узнать подробности об ошибках\n' +
+            '/del message - узнать подробности об удаленных сообщениях\n' +
+            '/unit-test - провести тестирование бота на работоспособность');
+        // all statistic:
+        // let static_all = [];
+        // log.forEach(function(element) {
+        //     static_all.push('`' + JSON.stringify(element) + '`');
+        // });
+        // bot.sendMessage(id, static_all.join(',\n '), { parse_mode: 'markdown' });
+        del_message = sqlite.run("SELECT count(*) as cont FROM log WHERE `type_log` == 'message'");
+        error_massage = sqlite.run("SELECT COUNT(*) as cont FROM log WHERE `type_log` == 'error'");
+        all_message = sqlite.run('SELECT COUNT(*) as cont FROM log');
+        console.log('date: ', moment(start[0].date).format('DD.MM.YYYY hh:mm:ss'));
+        console.log('del_message: ', del_message[0].cont);
+        statistic = 'Статистика c \'' + moment(start[0].date).format('DD.MM.YYYY hh:mm:ss') + '\' по \'' + moment(datetime).format('DD.MM.YYYY hh:mm:ss') +
+            '\' \n удалено сообщений: ' + del_message[0].cont +
+            '\n вызвано ошибок: ' + error_massage[0].cont +
+            '\n всего сообщений в базе: ' + all_message[0].cont;
+        bot.sendMessage(id, statistic, { parse_mode: 'markdown' });
     }
 });
 
+// filter message
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const id = msg.from.id;
@@ -49,6 +82,7 @@ bot.on('message', (msg) => {
                     chat_id: chatId,
                     message_id: msg.message_id,
                     type: "del message type = 'text'",
+                    type_log: 'message',
                     message: JSON.stringify(msg),
                     error: '',
                     date: datetime
@@ -68,6 +102,7 @@ bot.on('message', (msg) => {
                 chat_id: chatId,
                 message_id: msg.message_id,
                 type: "del message not type 'text'",
+                type_log: 'message',
                 message: JSON.stringify(msg),
                 error: '',
                 date: datetime
@@ -84,6 +119,7 @@ bot.on('polling_error', (error) => {
         chat_id: chatId,
         message_id: msg.message_id,
         type: "error",
+        type_log: 'error',
         message: JSON.stringify(msg),
         error: JSON.stringify(error),
         date: datetime
@@ -97,6 +133,7 @@ bot.on('webhook_error', (error) => {
         chat_id: chatId,
         message_id: msg.message_id,
         type: "error",
+        type_log: 'error',
         message: JSON.stringify(msg),
         error: JSON.stringify(error),
         date: datetime
