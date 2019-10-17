54adbbb488b0af70811792d355c8a76f0da22d84
Daemon <makushin.dima@gmail.com>
2019-10-17 16:56:19 +0300

    up: bot, up sqllite

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const data = require('./config.json');
 // data for work bot
 const token = data.token;
 const admins = data.admins_user;
+const commands = data.commands;
 
 const bot = new TelegramBot(token, { polling: true, filepath: false });
 
@@ -15,6 +16,11 @@ if (!token) {
 
 // add sql-lite database
 const sqlite = require('sqlite-sync');
+
+let user_id = 123456789;
+let chat_Id = 123456789;
+let messages_id = 0;
+let messages = 'example';
 
 sqlite.connect('statistic.db');
 sqlite.run(`CREATE TABLE IF NOT EXISTS log(
@@ -34,7 +40,11 @@ sqlite.run(`CREATE TABLE IF NOT EXISTS log(
 
 // setting bot
 bot.onText(/\/setting/, (msg) => {
-    if (admins.indexOf(msg.from.id) > 0) {
+    user_id = msg.from.id;
+    chat_Id = msg.chat.id;
+    messages_id = msg.message_id;
+    messages = msg;
+    if (admins.indexOf(msg.from.id) >= 0) {
         let text = 'Список команд:\n' +
             '/setting - вызов данного меню;\n' +
             '/statistic - вся статистика за весь период работы;\n' +
@@ -48,7 +58,11 @@ bot.onText(/\/setting/, (msg) => {
 // statistic all
 bot.onText(/\/statistic/, (msg) => {
     let datetime = Date.now();
-    if (admins.indexOf(msg.from.id) > 0) {
+    user_id = msg.from.id;
+    chat_Id = msg.chat.id;
+    messages_id = msg.message_id;
+    messages = msg;
+    if (admins.indexOf(msg.from.id) >= 0) {
         const start = sqlite.run('SELECT date FROM log WHERE `id` = 1');
         let statistic = '';
         let del_message = sqlite.run("SELECT count(*) as cont FROM log WHERE `type_log` == 'message'");
@@ -58,33 +72,45 @@ bot.onText(/\/statistic/, (msg) => {
             '\' \n удалено сообщений: ' + del_message[0].cont +
             '\n вызвано ошибок: ' + error_massage[0].cont +
             '\n всего сообщений в базе: ' + all_message[0].cont;
-        bot.sendMessage(msg.from.id, statistic);
+        bot.sendMessage(user_id, statistic, { parse_mode: 'markdown' });
     }
 });
 
 bot.onText(/\/del/, (msg) => {
+    user_id = msg.from.id;
+    chat_Id = msg.chat.id;
+    messages_id = msg.message_id;
+    messages = msg;
     let del_massage = sqlite.run("SELECT * FROM log WHERE `type_log` == 'message'");
     console.log(JSON.stringify(del_massage));
-    // bot.sendMessage(msg.from.id, 'del: ' + JSON.stringify(del_massage));
+    bot.sendMessage(msg.from.id, 'находится в разработке!');
 });
 
 bot.onText(/\/error/, (msg) => {
+    user_id = msg.from.id;
+    chat_Id = msg.chat.id;
+    messages_id = msg.message_id;
+    messages = msg;
     let error_massage = sqlite.run("SELECT * FROM log WHERE `type_log` == 'error'");
     console.log(JSON.stringify(error_massage));
-    // bot.sendMessage(msg.from.id, 'error: ' + JSON.stringify(error_massage));
+    bot.sendMessage(msg.from.id, 'находится в разработке!');
 });
 
 // filter message
 bot.on('message', (msg) => {
+    user_id = msg.from.id;
+    chat_Id = msg.chat.id;
+    messages_id = msg.message_id;
+    messages = msg;
     const chatId = msg.chat.id;
     const id = msg.from.id;
-    let datetime = Date.now();
+    let datetime = Date.now(); commands
     if (
         (id !== admins[0]) &&
         (id !== admins[1])
     ) {
         if (msg.text) {
-            if (msg.text === '/setting') {
+            if (commands.indexOf(msg.text) >= 0) {
                 bot.deleteMessage(chatId, msg.message_id);
                 return
             }
@@ -98,17 +124,17 @@ bot.on('message', (msg) => {
                     message: JSON.stringify(msg),
                     error: '',
                     date: datetime
-                    // }, function (res) {
-                    //     if (res.error)
-                    //         throw res.error;
-                    //     console.log(res);
+                }, function (res) {
+                    if (res.error)
+                        throw res.error;
+                    console.log(res);
                 });
                 bot.deleteMessage(chatId, msg.message_id);
             } else {
                 if (msg.reply_markup) {
                     bot.deleteMessage(chatId, msg.message_id);
                 } else {
-                    console.log(JSON.stringify(msg));
+                    console.log('JSON.stringify(msg)');
                 }
             }
         } else {
@@ -134,37 +160,18 @@ bot.on('message', (msg) => {
 
 bot.on('polling_error', (error) => {
     let datetime = Date.now();
-    console.log('|++++|');
-    // console.log('chatId: ', message);
-    // sqlite.insert("log", {
-    //     id_user: msg.from.id,
-    //     chat_id: msg.chat.id,
-    //     message_id: msg.message_id,
-    //     type: "error",
-    //     type_log: 'error',
-    //     message: JSON.stringify(msg),
-    //     error: JSON.stringify(error),
-    //     date: datetime
-    // });
-    // bot.on('message', (msg) => {
-    //     console.log('msg', JSON.stringify(msg));
-
-    // });
-    // bot.on('message', (msg) => {
-    //     console.log('++++++++++++++++');
-    //     let datetime = Date.now();
-    //     sqlite.insert("log", {
-    //         id_user: msg.from.id,
-    //         chat_id: msg.chat.id,
-    //         message_id: msg.message_id,
-    //         type: "error",
-    //         type_log: 'error',
-    //         message: JSON.stringify(msg),
-    //         error: JSON.stringify(error),
-    //         date: datetime
-    //     });
-    //     console.log(error.code); // => 'EFATAL'
-    // });
+    // console.log('|++++|');
+    console.log('chatId: ', message);
+    sqlite.insert("log", {
+        id_user: user_id,
+        chat_id: chat_Id,
+        message_id: messages_id,
+        type: "error",
+        type_log: 'error',
+        message: JSON.stringify(messages),
+        error: JSON.stringify(error),
+        date: datetime
+    });
     console.log(error); // => 'EFATAL'
 });
 
